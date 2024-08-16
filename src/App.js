6b385425a9a9f3c5e7e6d994d46388ac4562376d
Mohsin Sheeraz <syedmohsinsheeraz123@gmail.com>
2024-08-16 06:21:04 +0500

    fix: "change button txt"

--- src/App.js
+++ src/App.js
@@ -560,10 +560,10 @@ export default function Home() {
           </Canvas>
           <div className='mt-6 flex justify-center absolute top-20 right-2'>
                 <button
-                  onClick={handleSave}
+                  // onClick={handleSave}
                   className='px-8 py-3 bg-[#273c75] text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out'
                 >
-                  Save Customized PNG
+                  Buy Now
                 </button>
               </div>
         </div>
