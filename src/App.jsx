cbf2d923eedc62045e013185f8f96faa94ca7912
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2024-08-31 02:31:14 +0500

    fix: "changes"

--- src/App.jsx
+++ src/App.jsx
@@ -1229,7 +1229,7 @@ const handleLogoChange = (event) => {
         }`}
       >
         <div className="w-full h-[8%] flex justify-center items-center">
-          <div className="w-full h-5/6 md:h-full absolute left-0 top-5 lg:-top-20 mb-56 lg:mb-0">
+          <div className="w-full h-4/5 lg:h-full absolute left-0 top-5 lg:-top-20 mb-56 lg:mb-0">
             <Canvas camera={{ position: [1, 0, 1], fov: 50 }} shadows>
               <Suspense fallback={null}>
                 <ambientLight intensity={0.6} />
@@ -1349,7 +1349,7 @@ const handleLogoChange = (event) => {
           </div>
         </div>
         <motion.div
-          className="border w-full h-[22%] md:h-[25%] absolute left-0 bottom-2 md:bottom-10 bg-white overflow-visible"
+          className="border w-full h-[28%] lg:h-[25%] absolute left-0 bottom-2 md:bottom-10 bg-white overflow-visible"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 50 }}
